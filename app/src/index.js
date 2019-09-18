bd94046f39b50acf0c438d7264ff8a4485addb4a
Fede Jinich <federico@iovlabs.org>
2019-09-18 16:05:17 -0300

    :ambulance: fix directories

--- app/src/index.js
+++ app/src/index.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './src/assets/styles/index.css';
-import App from './src/components/App';
-import * as serviceWorker from './src/serviceWorker';
+import './assets/styles/index.css';
+import App from './components/App';
+import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(<App />, document.getElementById('root'));
 
