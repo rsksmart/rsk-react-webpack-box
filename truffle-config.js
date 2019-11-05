842862dc0770d26f196a7bb1914530b930435bbc
dulcedu <dulce@gmail.com>
2019-11-04 21:30:40 -0800

    Add env variable to avoid deploy mnemonics into production

--- truffle-config.js
+++ truffle-config.js
@@ -1,11 +1,18 @@
 const HDWalletProvider = require('truffle-hdwallet-provider');
 
-//Put your mnemonic here, take care of this and don't deploy your mnemonic into production!
-const mnemonic = 'A_MNEMONIC';
+//Put your mnemonic in the .env
+//1) Install dotenv dependecy (npm i dotenv --save)
+//2) Create .env file in the root directory 
+//3) Require dotenv
+
+require('dotenv').config();
+//You should never deploy your mnemonic into production or you will lost your crypto!
+const mnemonic = process.env.A_MNEMONIC;
+//4) Make sure you add  togitignore the file .env 
 
 module.exports = {
   /**
-   * Networks define how you connect to your ethereum client and let you set the
+   * Networks define how you connect to your RSK client (same as Ethereum client) and let you set the
    * defaults web3 uses to send transactions. If you don't specify one truffle
    * will spin up a development blockchain for you on port 9545 when you
    * run `develop` or `test`. You can ask a truffle command to use a specific
